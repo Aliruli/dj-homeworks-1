505b8c20690f25723e92925da9773ea1bb802508
Model1.01 <model1.01@MacBook-Air-mac.local>
2023-05-25 02:05:27 +0300

    My first commit

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,17 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
+import os
+import datetime
 
 
-def home_view(request):
+def home(request):
+    """Функция обработки адресов и вывода в шаблон, списка ссылок."""
     template_name = 'app/home.html'
     # впишите правильные адреса страниц, используя
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('current_time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
-    
+
     # context и параметры render менять не нужно
     # подбробнее о них мы поговорим на следующих лекциях
     context = {
@@ -20,16 +23,20 @@ def home_view(request):
     return render(request, template_name, context)
 
 
-def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
+def current_time(request):
+    """Функция обработки и вывода на страницу текущего времени."""
+    # обратите внимание – здесь HTML шаблона нет,
     # возвращается просто текст
-    current_time = None
+    current_time = datetime.datetime.now()
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
 
 
-def workdir_view(request):
+def workdir(request):
+    """Функция обработки и вывода на страницу, списка файлов из дирректории."""
     # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
+    # который возвращает список файлов в рабочей
     # директории
-    raise NotImplemented
+    files = os.listdir('.')
+    files_list = '<br>'.join(files)
+    return HttpResponse(files_list)
--- 1.1-first-project/first_project/first_project/settings.py
+++ 1.1-first-project/first_project/first_project/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'first_project.urls'
 
+# TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
